608c935d14a49903da7364627d4f8c8fd57fbb64
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2025-04-30 17:03:24 +0200

    Replace tinyxml2_vendor with tinyxml2 in CMake configuration

--- kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
+++ kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
-find_dependency(tinyxml2_vendor REQUIRED)
+find_dependency(tinyxml2 REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-kss-client-libraryTargets.cmake")
