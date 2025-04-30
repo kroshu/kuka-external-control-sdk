bea94cefa3f41b5b4a3f4d0b0e21ac6e09a060e6
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2025-04-30 17:24:39 +0200

    Update CMake configuration to use tinyxml2 and fallback to tinyxml2_vendor if not found

--- kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
+++ kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
@@ -1,6 +1,12 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
-find_dependency(tinyxml2 REQUIRED)
+find_dependency(tinyxml2 QUIET)
+
+if(NOT tinyxml2_FOUND)
+  message(STATUS "tinyxml2 not found")
+  find_package(tinyxml2_vendor REQUIRED)
+endif()
+
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-kss-client-libraryTargets.cmake")
