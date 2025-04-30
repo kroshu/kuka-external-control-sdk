0053182dbda401be0ca6e70c3d37177f3f5ee08c
Kristóf Pásztor <pasztor.kristof.kp@gmail.com>
2025-04-30 17:36:47 +0200

    Remove tinyxml2 dependency

--- kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
+++ kuka_external_control_sdk/kss/cmake/kuka-kss-client-libraryConfig.cmake
@@ -1,12 +1,5 @@
 include(CMakeFindDependencyMacro)
 
 find_dependency(Threads REQUIRED)
-find_dependency(tinyxml2 QUIET)
-
-if(NOT tinyxml2_FOUND)
-  message(STATUS "tinyxml2 not found")
-  find_dependency(tinyxml2_vendor REQUIRED)
-endif()
-
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-kss-client-libraryTargets.cmake")
