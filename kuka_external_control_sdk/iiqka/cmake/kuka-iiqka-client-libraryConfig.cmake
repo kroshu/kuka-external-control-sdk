d7d19dd28b2ccab687293ec04d3bf3098f3f175a
kovacsge11 <kovacsge11@gmail.com>
2025-12-10 13:07:58 +0100

    remove non-kss related stuff

--- kuka_external_control_sdk/iiqka/cmake/kuka-iiqka-client-libraryConfig.cmake
+++
@@ -1,6 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-find_dependency(OpenSSL REQUIRED)
-find_dependency(Threads REQUIRED)
-
-include("${CMAKE_CURRENT_LIST_DIR}/kuka-iiqka-client-libraryTargets.cmake")
