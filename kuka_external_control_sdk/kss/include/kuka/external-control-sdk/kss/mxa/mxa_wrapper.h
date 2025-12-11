99ab8d12b8451a4a8ab05920f7ee9c5e499499d7
Svastits <49677296+Svastits@users.noreply.github.com>
2025-12-11 09:51:51 +0100

    fix auto PR commit

--- kuka_external_control_sdk/kss/include/kuka/external-control-sdk/kss/mxa/mxa_wrapper.h
+++ kuka_external_control_sdk/kss/include/kuka/external-control-sdk/kss/mxa/mxa_wrapper.h
@@ -77,17 +77,15 @@ public:
   }
 
 void getmxAOutput(unsigned char *read_buffer) {
-    if (read_buffer == nullptr) return; // Or handle error
+    if (read_buffer == nullptr) return;
     krc_read_.KRC4_INPUT = read_buffer;
-    // Ensure the KRC_READAXISGROUP instance knows the buffer size
     krc_read_.OnCycle();
   }
 
   void setmxAInput(unsigned char *write_buffer) {
-    if (write_buffer == nullptr) return; // Or handle error
+    if (write_buffer == nullptr) return;
     krc_write_.KRC4_OUTPUT = write_buffer;
     krc_write_.OnCycle();
-  }
   }
 
   int mxACycle() {
