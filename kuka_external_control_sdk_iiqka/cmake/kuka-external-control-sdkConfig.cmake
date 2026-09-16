03912a956debc97518d982ebe0e7949b41f775ec
Svastits <svastits1@gmail.com>
2026-09-16 10:28:56 +0200

    build fixes

--- kuka_external_control_sdk_iiqka/cmake/kuka-external-control-sdkConfig.cmake
+++ kuka_external_control_sdk_iiqka/cmake/kuka-external-control-sdkConfig.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 
+find_dependency(kuka_external_control_sdk_common REQUIRED)
 find_dependency(kuka-iiqka-client-library REQUIRED)
-find_dependency(kuka-kss-client-library REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/kuka-external-control-sdkTargets.cmake")
--- kuka_external_control_sdk_iiqka/iiqka/cmake/kuka-iiqka-client-libraryConfig.cmake
+++ kuka_external_control_sdk_iiqka/iiqka/cmake/kuka-iiqka-client-libraryConfig.cmake
@@ -1,5 +1,6 @@
 include(CMakeFindDependencyMacro)
 
+find_dependency(kuka_external_control_sdk_common REQUIRED)
 find_dependency(OpenSSL REQUIRED)
 find_dependency(Threads REQUIRED)
 
